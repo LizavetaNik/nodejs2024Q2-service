f338abd09a6cbebbcfe644ca9df391986e89c9d9
Lizaveta <lizabeth.nik@gmail.com>
2024-02-29 11:56:58 +0200

    feat: add start server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import * as yaml from 'js-yaml';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const fileAPI = fs.readFileSync(
+    path.join(__dirname, '../doc/api.yaml'),
+    'utf8',
+  );
+  const docfileAPI = yaml.load(fileAPI);
+
+  SwaggerModule.setup('doc', app, docfileAPI as OpenAPIObject);
+  await app.listen(PORT);
 }
 bootstrap();
