8a230605d53063db7ec7271348a1e8f3474e135e
Lizaveta <lizabeth.nik@gmail.com>
2024-03-04 13:45:12 +0200

    fix: add db

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,15 @@
-import { ITrack, IUser } from 'src/types/types';
+import { IAlbum } from 'src/album/album-types';
+import { IArtist } from 'src/artist/artist-types';
+import { IFavorites } from 'src/favorites/favorites-types';
+import { ITrack } from 'src/track/track-types';
+import { IUser } from 'src/user/user-types';
 
 export const usersDb: IUser[] = [];
 export const trackDb: ITrack[] = [];
+export const albumDb: IAlbum[] = [];
+export const artistsDb: IArtist[] = [];
+export const favoritesDb: IFavorites = {
+  albums: [],
+  artists: [],
+  tracks: [],
+};
