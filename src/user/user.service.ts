0fd9dcbe379f2c1134504ef8e461235ff11e452f
Lizaveta <lizabeth.nik@gmail.com>
2024-03-08 11:31:11 +0200

    fix: fix import user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,7 +8,6 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user.dto';
 import { DatabaseService } from 'src/database/database.service';
 import { omit } from 'lodash';
-import { compare, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
