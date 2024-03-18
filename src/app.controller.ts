975061ad5340c2841dfbd8fd33664c02fae2c778
Lizaveta <lizabeth.nik@gmail.com>
2024-03-18 14:00:53 +0200

    feat: init user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/user')
+  getUsers() {
+    return this.appService.getUsers();
+  }
+
+  @Get('/user/:userId')
+  getUser(@Param('userId', ParseUUIDPipe) userId: string) {
+    return this.appService.getUser(userId);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DatabaseService } from './database/database.service';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  getUsers() {
+    return this.databaseService.user.findMany();
+  }
+  getUser(userId: string) {
+    const user = this.databaseService.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('This user does not exist');
+    }
+    return user;
+  }
+}
