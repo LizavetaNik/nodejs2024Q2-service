703e399e3623086518920d08f5af7f158b1f3369
Lizaveta <lizabeth.nik@gmail.com>
2024-03-27 16:16:57 +0200

    feat: add exception.filter

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import * as path from 'node:path';
 import * as fs from 'node:fs';
 import * as yaml from 'js-yaml';
 import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './log/logger.exception.filter';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   const fileAPI = fs.readFileSync(
     path.join(__dirname, '../doc/api.yaml'),
     'utf8',
